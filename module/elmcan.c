fb0885b00da32c43890ed0035cfc2a7c172c48cc
norly <ny-git@enpas.org>
2019-02-19 22:56:48 +0100

    Style: Move elm327_is_ready_char() further down to where it is used

--- module/elmcan.c
+++ module/elmcan.c
@@ -371,15 +371,6 @@ static inline void elm327_hw_failure(struct elmcan *elm)
   * (assumes elm->lock taken)					*
   ************************************************************************/
 
-static bool elm327_is_ready_char(char c)
-{
-	/* Bits 0xc0 are sometimes set (randomly), hence the mask.
-	 * Probably bad hardware.
-	 */
-	return (c & 0x3f) == ELM327_READY_CHAR;
-}
-
-
 static void elm327_parse_error(struct elmcan *elm, int len)
 {
 	struct can_frame frame;
@@ -679,6 +670,15 @@ static void elm327_handle_prompt(struct elmcan *elm)
 		elm327_send(elm, "ATMA\r", 5);
 		elm->state = ELM_RECEIVING;
 	}
+}
+
+
+static bool elm327_is_ready_char(char c)
+{
+	/* Bits 0xc0 are sometimes set (randomly), hence the mask.
+	 * Probably bad hardware.
+	 */
+	return (c & 0x3f) == ELM327_READY_CHAR;
 }
 
 
